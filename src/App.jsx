a94c23996fea03f9145aa1230575260f01d9da9f
Digudev <1.5.dg.diego@gmail.com>
2023-01-24 10:13:09 -0600

    App SetUp

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './style';
+
+const App = () => {
+	return <div className="bg-primary w-full overflow-hidden">HooBank App</div>;
+};
+
+export default App;
