311bd239c154225aa52d7714dc5080d94d783e51
Digudev <1.5.dg.diego@gmail.com>
2023-01-24 10:37:33 -0600

    Layout and componets created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials } from './components';
 import style from './style';
 
 const App = () => {
-	return <div className="bg-primary w-full overflow-hidden">HooBank App</div>;
+	return (
+		<div className="bg-primary w-full overflow-hidden">
+			<div className={`${style.paddingX} ${style.flexCenter}`}>
+				<div className={`${style.boxWidth}`}>
+					<Navbar />
+				</div>
+			</div>
+
+			<div className={`bg-primary ${style.flexStart} ${style.paddingX}`}>
+				<div className={`${style.boxWidth}`}>
+					<Stats />
+					<Business />
+					<Billing />
+					<CardDeal />
+					<Testimonials />
+					<Clients />
+					<CTA />
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
